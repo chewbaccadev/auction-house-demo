49945ab06d69bdfc763c75c49e321b2a6b73cb78
Vlad <vlad.cainamisir13@gmail.com>
2023-09-30 16:22:28 +0300

    Create pages

---
+++ src/app/buy/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const Buy = (props: Props) => {
+  return <div>Buy</div>;
+};
+
+export default Buy;
---
+++ src/app/mint/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const Mint = (props: Props) => {
+  return <div>Mint</div>;
+};
+
+export default Mint;
